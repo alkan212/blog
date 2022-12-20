9d39d14609785357792b241be4da6db561805d85
alkan212 <alkanakdeniz21@gmail.com>
2022-12-20 19:33:49 +0100

    a

--- src/pages/template/advice/howtomakeyourhamsterhappy.jsx
+++ src/pages/template/advice/howtomakeyourhamsterhappy.jsx
@@ -1,5 +1,5 @@
 // a changer !
-const titreOnglet = "  how to have a happy hamster "
+const titreOnglet = "  how to have a happy Xhamster "
 const description = "Hamsters are cute and entertaining pets that can bring joy to their owners. Here are some tips for keeping your hamster happy."
 
 const sujet = "advice"
