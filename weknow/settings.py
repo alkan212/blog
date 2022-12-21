6a1ff0eeff4dbaee7f6918d3a95d1f8e2b3bd88c
alkan212 <alkanakdeniz21@gmail.com>
2022-12-21 14:48:50 +0100

    set settings

--- weknow/settings.py
+++ weknow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vqd3iyr-b1lg6ojm#ysppc=)!9r(yl&5ju+p9=lbt2iv#cqi^@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "weknowblog.herokuapp.com"]
 
 
 # Application definition
